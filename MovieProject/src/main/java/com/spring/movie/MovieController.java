63d9d090253cacf62e088cc6c7bbc3442e0422b7
Yujeen Han <jeenstar11@gmail.com>
2019-07-17 10:37:15 +0900

    개발환경 설정
    1. 7/16 git-userCombine에서 pull한 프로젝트 import 및 개발환경 세팅
    2. LoginController 패키지 분리 및 파일 수정 (순서변경, 주석추가)

--- MovieProject/src/main/java/com/spring/movie/MovieController.java
+++ MovieProject/src/main/java/com/spring/movie/MovieController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MovieController {
 	
+	//영화 - 영화리스트
 	@RequestMapping(value="/movieList.do", method=RequestMethod.GET)
 	public String movieList() {
 		return "movie/movieList";
 	}
 	
+	//영화 - 영화상세정보 
 	@RequestMapping(value="/movieDetail.do", method=RequestMethod.GET)
 	public String moveDetail() {
 		return "movie/movieDetail";
---
+++ MovieProject/src/main/java/com/spring/other/BoardFreeController.java
@@ -0,0 +1,26 @@
+package com.spring.other;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/free/*")
+
+public class BoardFreeController {
+	
+	//
+	@GetMapping("boardFreeList.do")
+	public void getListPage() {}
+	@GetMapping("boardFreeGet.do")
+	public void getGetPage() {}
+	@GetMapping("boardFreeWrite.do")
+	public void getWritePage() {}
+	@GetMapping("boardFreeUpdate.do")
+	public void getUpdatePage() {}
+	@GetMapping("boardFreeList_test.do")
+	public void getList_testPage() {}
+	
+	
+	
+}//e_BoardFreeController class
